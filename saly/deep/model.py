77aea977bfb566aed95d1f26bb4aaf27ab9eefff
LongarMD <mark.longar@gmail.com>
2019-08-30 16:16:00 +0200

    Fixes PD errors

--- saly/deep/model.py
+++ saly/deep/model.py
@@ -30,7 +30,7 @@ def build_model(data, markers, bottleneck_dim=25, intermediate_dim=100, dropout_
 
     # -- Model --
     input_layer = Input(shape=(input_dim,))
-    partial_input = Partial(partial_dim, weight_mask=partially_dense_mask, use_bias=True,
+    partial_input = Partial(partial_dim, weight_mask=partially_dense_mask, use_bias=True, kernel_initializer='ones',
                             activation=activation)(input_layer)
     marker_layer = Partial(marker_dim, weight_mask=weight_mask, use_bias=False, kernel_initializer='ones',
                            activation=activation, name='cell_activations')(partial_input)
