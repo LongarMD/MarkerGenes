67eb11b839fa37a5652ddad11127bc72e5c45e1c
LongarMD <mark.longar@gmail.com>
2020-01-18 21:20:52 +0100

    Formatting

--- saly/backend/analysis.py
+++ saly/backend/analysis.py
@@ -2,6 +2,10 @@ import pickle
 from random import uniform, seed
 from time import time
 from collections import OrderedDict
+from saly import backend
+import pandas as pd
+from sklearn import metrics
+import numpy as np
 
 
 def get_top_activated_indices(n: int, cell_type_activations: list) -> list:
@@ -76,3 +80,35 @@ def get_graph_labels(handles, plt_labels):
     by_label = OrderedDict(sorted(by_label.items()))
 
     return by_label.values(), by_label.keys()
+
+
+def get_class_evaluation(labels, cell_activations, markers, aliases):
+    cell_types = backend.get_cell_types(markers)
+    top_activations = backend.get_top_activated_indices(1, cell_activations)
+
+    predictions = backend.index_to_cell_type(top_activations, cell_types)
+    predictions = pd.Series([p[0] for p in predictions])
+
+    y_true = labels
+    y_true = y_true.apply(lambda x: aliases[x] if x in aliases.keys() else x)
+    unique_classes = sorted(y_true.append(predictions).unique())
+    true_classes = sorted(y_true.unique())
+
+    cm = metrics.confusion_matrix(y_true, predictions)
+
+    fp = cm.sum(axis=0) - np.diag(cm)
+    fn = cm.sum(axis=1) - np.diag(cm)
+    tp = np.diag(cm)
+    tn = cm.sum() - (fp + fn + tp)
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1_score = 2 * (recall * precision) / (recall + precision)
+
+    results = {}
+    for i, cls in enumerate(unique_classes):
+        if cls in true_classes:
+            result = {'tp': tp[i], 'tn': tn[i], 'fp': fp[i], 'fn': fn[i],
+                      'precision': precision[i], 'recall': recall[i], 'f1': f1_score[i]}
+            results.update({cls: result})
+    return results
